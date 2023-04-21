bcf10c5507ef4e4c257bfdc68b4290bd6b902e24
Ashlesh Dhanvalkar <ashudhanvalkar2020@gmail.com>
2023-04-21 11:18:19 +0530

    Create First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
+import Cart from "./component/Cart";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Cart />
+    </Provider>
+  );
+}
+
+export default App;
